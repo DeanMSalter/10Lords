d0929f468a4c203b67a286434a5eb808c6f7ba29
up856990 <45265933+up856990@users.noreply.github.com>
2019-03-29 00:41:46 +0000

    Update reset-password.php
    Added CSS

--- app/reset-password.php
+++ app/reset-password.php
@@ -76,31 +76,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Reset Password</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+    <link href='css/register-login-style.css' rel='stylesheet' type='text/css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 </head>
 <body>
     <div class="wrapper">
+      <div class = 'BlackBox' id ="centre_BlackBox">
+      <div class = 'move-Content' id = "move-Content">
+        <img src="goldLogoo.jpg" width="350" height="350" class = "img-center">
         <h2>Reset Password</h2>
         <p>Please fill out this form to reset your password.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
-                <label>New Password</label>
+                <p1>New Password</p1>
                 <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
                 <span class="help-block"><?php echo $new_password_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
+                <p1>Confirm Password</p1>
                 <input type="password" name="confirm_password" class="form-control">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group" id = "center_buttons">
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <a class="btn btn-link" href="welcome.php">Cancel</a>
             </div>
         </form>
+      </div>
+    </div>
     </div>
 </body>
 </html>
