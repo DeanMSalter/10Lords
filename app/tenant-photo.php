c5db567886d037fb990f1562dd8707549a849343
leo-98 <up817852@myport.ac.uk>
2019-03-31 17:58:04 +0100

    Add files via upload
    photo upload feature added

---
+++ app/tenant-photo.php
@@ -0,0 +1,150 @@
+<?php
+
+  require_once "config.php";
+  session_start();
+
+  // Check if the user is logged in, if not then redirect him to login page
+  if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+      header("location: index.html");
+      exit;
+  }
+  // Initialize message variable
+  $msg = "";
+
+  // If upload button is clicked ...
+  if (isset($_POST['upload'])) {
+  	// Get image name
+  	$image = $_FILES['image']['name'];
+  	// Get text
+  	$image_text = mysqli_real_escape_string($link, $_POST['image_text']);
+    $property_id = $_GET["property_id"];
+
+  	// image file directory
+  	$target = "images/";
+
+  	$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+  	// execute query
+  	mysqli_query($link, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+  }
+  $result = mysqli_query($link, "SELECT * FROM images");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="shortcut icon" href="ico2.ico" type="image/x-icon">
+  <meta charset="utf-8">
+  <title>Landlord Home</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+  <link rel="stylesheet" href="css/myStyle.css">
+  <link href='css/navStyle.css' rel='stylesheet' type='text/css'>
+  <script src="js/script.js"></script>
+  <link rel="css/stylesheet" href="index-style.css">
+</head>
+
+<body>
+  <header class="stickyNav">
+    <div class="mainHeader background-box background-color">
+      <div class="mainHeader-grid fs">
+        <div class="grid-column-33-per content-align-left">
+          <div class="menuNavButton">
+            <span onclick="openNav()">
+              <img class="menuNavImg" width="25" height="25" src="MenuNav.png" alt="Menu Navigation Button">
+            </span>
+          </div>
+          <nav id="navigationBar" class="sideNav background-color">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <div class="navContainer">
+              <ul class="navMenu">
+                <li class="navItems">
+                  <a href="tenant-home.php">Home</a>
+                </li>
+                <li class="navItems">
+                  <a href="chat.php">Chat</a>
+                </li>
+                <li class="navItems">
+                  <a>Acount</a>
+                </li>
+                <li class="navItems">
+                  <a href ="property.php"> Property Management</a>
+                </li>
+                <li class="navItems">
+                  <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+                </li>
+                <li class="navItems">
+                  <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+                </li>
+              </ul>
+              <footer>
+                <p>something</p>
+              </footer>
+            </div>
+          </nav>
+        </div>
+        <div class="grid-column-33-per content-align-center">
+          <div class="headerLogo">
+            <p>logo</p>
+          </div>
+        </div>
+        <div class="grid-column-33-per content-align-right">
+          <div class="socialLayout">
+            <ul>
+              <li>
+                <a href="https://twitter.com/leoclarke_" target="_blank">
+                  <img height="20" width="20" src="socials/twitter.png" alt="Twitter Icon">
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/leoclarke_/" target="_blank">
+                  <img height="20" width="20" src="socials/instagramPNG.png" alt="Instagram Icon">
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/leo-clarke-663315157/" target="_blank">
+                  <img height="20" width="20" src="socials/linkedinPNG.png" alt="Linkedin Icon">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div id="main" class="main">
+    <h1>photo upload</h1>
+    <?php
+    while ($row = mysqli_fetch_array($result)) {
+      echo "<div id='img_div'>";
+      echo "<img src='images/".$row['image']."' >";
+      echo "<p>".$row['image_text']."</p>";
+      echo "</div>";
+    }
+    ?>
+    <form method="POST" action="tenant-photo.php" enctype="multipart/form-data">
+      <input type="hidden" name="size" value="1000000">
+      <div>
+        <input type="file" name="image">
+      </div>
+      <div>
+        <textarea
+        id="text"
+        cols="40"
+        rows="4"
+        name="image_text"
+        placeholder="Say something about this image..."></textarea>
+      </div>
+      <div>
+        <button type="submit" name="upload">POST</button>
+      </div>
+    </form>
+
+  </div>
+</body>
+</html>
