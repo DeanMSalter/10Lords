6e67cbf0523418f5bd14536015ca5728f89c3d34
leo-98 <up817852@myport.ac.uk>
2019-04-02 12:38:15 +0100

    Update tenant-home.php

--- app/tenant-home.php
+++ app/tenant-home.php
@@ -1,6 +1,7 @@
 <?php
 // Initialize the session
 session_start();
+require_once "config.php";
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
