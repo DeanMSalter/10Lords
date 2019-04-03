e81edade8829b4e2ee09c1b7aaead00019319527
Dean Salter <dean.m.salter@gmail.com>
2019-04-03 13:47:34 +0100

    deleted social links and adjusted database tables

--- app/chat.php
+++ app/chat.php
@@ -80,29 +80,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             <p>logo</p>
           </div>
         </div>
-
-        <div class="grid-column-33-per content-align-right">
-          <div class="socialLayout">
-            <ul>
-              <li>
-                <a href="https://twitter.com/leoclarke_" target="_blank">
-                  <img height="20" width="20" src="socials/twitter.png" alt="Twitter Icon">
-                </a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/leoclarke_/" target="_blank">
-                  <img height="20" width="20" src="socials/instagramPNG.png" alt="Instagram Icon">
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/leo-clarke-663315157/" target="_blank">
-                  <img height="20" width="20" src="socials/linkedinPNG.png" alt="Linkedin Icon">
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
       </div>
     </div>
   </header>
